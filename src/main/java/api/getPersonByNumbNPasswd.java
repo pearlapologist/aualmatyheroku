be484d7432e2edaf71083e25262b7aa285b35649
bayan <addict.hamster@gmail.com>
2020-06-26 12:45:21 +0600

    getperson

--- src/main/java/api/getPersonByNumbNPasswd.java
+++ src/main/java/api/getPersonByNumbNPasswd.java
@@ -32,8 +32,7 @@ public class getPersonByNumbNPasswd extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -63,38 +62,41 @@ public class getPersonByNumbNPasswd extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-        
+
         PrintWriter out = response.getWriter();
-        
-         JsonReader jsonReader = Json.createReader(request.getReader());
 
-            JsonObject jsonObject = jsonReader.readObject();
+        JsonReader jsonReader = Json.createReader(request.getReader());
 
-            String number = jsonObject.getString("numb");
-            String passwd = jsonObject.getString("passwd");
+        JsonObject jsonObject = jsonReader.readObject();
 
-          Person p = new Person();
-            p.setNumber(number);
-            p.setPasswd(passwd);
+        String number = jsonObject.getString("numb");
+        String passwd = jsonObject.getString("passwd");
 
-            models.DbHelper db = new models.DbHelper();
-         Person person = db.getPersonByPasswdNNumb(number, passwd);
+        Person p = new Person();
+        p.setNumber(number);
+        p.setPasswd(passwd);
 
-        
-        if(person == null){
-           JsonObjectBuilder objectBuilder = Json.createObjectBuilder().add("errorMessage", "404error");
+        models.DbHelper db = new models.DbHelper();
+        Person person = db.getPersonByPasswdNNumb(number, passwd);
+
+        if (person == null) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder().add("errorMessage", "404error");
             String result = objectBuilder.build().toString();
             out.print(result);
             return;
         }
-         JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
-          objectBuilder.add("personId", person.getId());
-           objectBuilder.add("name", person.getName());
-            objectBuilder.add("lastname", person.getLastname());
-           objectBuilder.add("number", person.getNumber());
-           
-           JsonObject jsonObject2  = objectBuilder.build();
-           out.print(jsonObject2.toString());
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        objectBuilder.add("id", person.getId());
+        objectBuilder.add("name", person.getName());
+        objectBuilder.add("lastname", person.getLastname());
+        objectBuilder.add("number", person.getNumber());
+        objectBuilder.add("rating", person.getRating());
+        String c = person.getCreatedDate() + "";
+        objectBuilder.add("created", c);
+        String b = person.getBirthday() + "";
+        objectBuilder.add("birth", b);
+        JsonObject jsonObject2 = objectBuilder.build();
+        out.print(jsonObject2.toString());
     }
 
     /**
